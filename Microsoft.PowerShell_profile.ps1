aa189bb6e0891e58af447b42721cd4e939e28a76
Richard <sh1nyfox@protonmail.com>
2025-08-20 15:48:20 +0100

    Added Intellisense to profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,16 @@
+# Load the PSReadLine module
+Import-Module PSReadLine
+
+# Set command history options for older PSReadLine versions
+Set-PSReadLineOption -HistorySavePath (Join-Path $env:APPDATA 'Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt')
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+
+# Enable predictive IntelliSense from history
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+
+# (Optional) Set the view style to ListView
+Set-PSReadLineOption -PredictionViewStyle ListView
+
 Invoke-Expression (&starship init powershell)
 
 fastfetch
